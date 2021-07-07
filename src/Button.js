9f7b760a7d469272b7803067aca419f71ccf8395
Anton <enitefall81@gmail.com>
2021-07-07 10:02:10 -0700

    Add Button design

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,16 @@
 export React from "react"
 
 
-export default function Button() {
-
+export default function Button({children, onClick}) {
+  return <button
+    style={{
+      border: "1px solid",
+      cursor: "pointer",
+      padding: "0.75em 0",
+      overflow: "hidden"
+    }}
+    onClick={onClick}
+  >
+    {children}
+  </button>
 }
